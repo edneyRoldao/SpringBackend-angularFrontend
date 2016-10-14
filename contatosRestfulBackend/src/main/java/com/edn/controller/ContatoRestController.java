70e8e3cca4faa7b0ad190038a4eae0fec71e43d5
edneyRoldao <edneyroldao@gmail.com>
2016-10-14 17:51:37 -0300

    test

--- contatosRestfulBackend/src/main/java/com/edn/controller/ContatoRestController.java
+++ contatosRestfulBackend/src/main/java/com/edn/controller/ContatoRestController.java
@@ -3,6 +3,8 @@ package com.edn.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,8 +38,15 @@ public class ContatoRestController {
 	}
 	
 	@GetMapping("/{id}")
-	public Contato findOne(@PathVariable("id") Integer id) {
-		return service.read(id);
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public ResponseEntity findOne(@PathVariable("id") Integer id) {
+		
+		Contato contato = service.read(id);
+
+		if(contato == null) 
+			return new ResponseEntity(contato, HttpStatus.NOT_FOUND); 
+		
+		return new ResponseEntity(contato, HttpStatus.OK);
 	}
 
 	@PostMapping
